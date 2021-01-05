87540bca44cc9b10303158bb94ce770504e87f89
Moko <moko@mokos-mac-mini.home>
2021-01-05 11:32:05 +0000

    ng g c laboratory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LaboratoryComponent } from './laboratory/laboratory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LaboratoryComponent
   ],
   imports: [
     BrowserModule
