af52d959d4396950381201d24afe1471027b81f3
Moko <moko@mokos-mac-mini.home>
2021-01-23 13:12:20 +0000

    Highlight nav-link nor working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,6 @@ import { LaboratoryService } from './laboratory/services/laboratory.service';
 })
 export class AppComponent {
 
-  public laboratory_isActive = true;
-  public development_isActive = false;
-  public shareKnowledge_isActive = false;
-  public photography_isActive = false;
-
   public photography_isCreated = false;
 
   constructor(
@@ -24,33 +19,17 @@ export class AppComponent {
 
   public cleanUp_development() {
       this.developmentService.cleanUp();
-      this.laboratory_isActive = true;
-      this.development_isActive = false;
-      this.shareKnowledge_isActive = false;
-      this.photography_isActive = false;
   }
 
   public cleanUp_laboratory() {
       this.laboratoryService.cleanUp();
-      this.laboratory_isActive = false;
-      this.development_isActive = true;
-      this.shareKnowledge_isActive = false;
-      this.photography_isActive = false;
   }
 
   public cleanUp_photography() {
       // this.photographyService.cleanUp();
-      // this.laboratory_isActive = false;
-      // this.development_isActive = false;
-      // this.shareKnowledge_isActive = false;
-      // this.photography_isActive = true;
   }
 
   public cleanUp_shareKnowledge() {
       // this.photographyService.cleanUp();
-      // this.laboratory_isActive = false;
-      // this.development_isActive = false;
-      // this.shareKnowledge_isActive = false;
-      // this.photography_isActive = true;
   }
 }
